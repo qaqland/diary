637c60f5d0a7ad61adede92532412193bf762aa4
qaqland <62464571+qaqland@users.noreply.github.com>
2022-01-07 20:29:02 +0800

    all done but panic when end

--- diary.go
+++ diary.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 
+	mapset "github.com/deckarep/golang-set"
 	git "github.com/libgit2/git2go/v31"
 )
+
+var had_show mapset.Set
 
 func main() {
 	diary, err := git.OpenRepository("./")
@@ -17,24 +20,36 @@ func main() {
 	if err != nil {
 		fmt.Println("lookup commit err:", err)
 	}
-	show(last_commit)
-
-	count := last_commit.ParentCount()
-	parent_commit := last_commit.Parent(0)
-	// 可能会很多 commit 产生一个 commit
-	// count = 0 走到了重点
-	// count = 1 有一个爸爸
-	// count = 2 有两个爸爸
-
-	var i uint
-	for i = 0; i < count; i++ {
-		parent_commit = parent_commit.Parent(0)
-		show(parent_commit)
-	}
-
+	had_show = mapset.NewSet()
+	parent(last_commit)
 }
+
 func show(commit *git.Commit) {
 	message := commit.Message()
 	date := commit.Author().When
 	fmt.Println(date, message)
 }
+
+func parent(commit *git.Commit) {
+	// 如果已经 show 过，就直接返回
+	if had_show.Contains(commit) {
+		return
+	}
+	had_show.Add(commit)
+	show(commit)
+	count := commit.ParentCount()
+	if count == 0 {
+		return
+	}
+	var i uint
+	// 可能会很多 commit 产生一个 commit
+	// count = 0 走到了终点
+	// count = 1 有一个爸爸
+	// count = 2 有两个爸爸
+	for i = 0; i < count; i++ {
+		commit := commit.Parent(i)
+		parent(commit)
+	}
+}
+
+// go build -tags static diary.go
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/libgit2/git2go/v31 v31.7.4
 
 require (
+	github.com/deckarep/golang-set v1.8.0
 	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
 	golang.org/x/sys v0.0.0-20201204225414-ed752295db88 // indirect
 )
